75424602bb6d36560dfdb8e78dd76dd90544f497
Brian Smith <brian.bubbam@gmail.com>
2021-07-21 09:29:27 -0500

    commit tree 273314ea1b2c121f6a187c29f2829e43f5a0e2cc

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,40 @@
+require "pry"
+
 class Customer
   
+    @@all = []
+
+    attr_accessor :name, :age
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip)
+        #make a new meal using the current waiter then pass through the three arugemnts and self to get the customer
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        # you get the meal class and the all array itterate it using select method and then see
+        # which customer is equal to self
+        Meal.all.select {|meal| meal.customer == self}
+    end
+
+    def waiters
+        # call on the meals method that already gives us a list of that customers meals itterate through
+        # that and return the waiters associated with the meals
+        meals.map do |meal|
+            meal.waiter
+        end
+    end
+
+
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,41 @@
 class Waiter
 
+    @@all = []
+
+    attr_accessor :name, :years
+
+    def initialize(name, years)
+        @name = name
+        @years = years
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        #initializes a meal using the current Waiter instance, a provided Customer instance and a total and tip
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        # returns an Array of Meal instances associated with this waiter
+        Meal.all.select do |meal|
+            meal.waiter == self
+        end
+    end
+
+    def best_tipper
+        #returns the Customer instance associated with the meal that received the largest tip (FAILED - 1)
+
+        # max method returns the biggest thing you are looking for in this case tip
+        best_tip = meals.max do |a, b|
+            a.tip <=> b.tip
+            
+        end
+        best_tip.customer
+    end
+
+
 end
